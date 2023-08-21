399aa6e6c5fa1e0158cb2da8fe74991f92b7520d
thandazanig <thandazanigwampa99@gmail.com>
2023-08-21 21:19:47 +0200

    "added build files"

---
+++ cmake-build-debug/CMakeFiles/QuestionOneProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuestionOneProject.dir/main.c.o"
+  "QuestionOneProject"
+  "QuestionOneProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuestionOneProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
